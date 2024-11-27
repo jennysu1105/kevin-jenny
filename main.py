af3de225aca494c4eaf66ccc866a2df9443ad16c
Jenny Su <71398958+jennysu1105@users.noreply.github.com>
2024-11-26 21:11:45 -0500

    slash command implementation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import datetime
 from dateutil.relativedelta import relativedelta
 from discord.ext import commands
+from typing import Optional
 from keep_alive import keep_alive
 from access_json import *
 import shutil
@@ -13,36 +14,38 @@ intents = discord.Intents.all()
 
 client = commands.Bot(command_prefix="kj!", intents=intents)
 
-
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game(
-        name="learn about commands with kj!h"))
-    print("Ready!")
-
-
+client.remove_command("help")
 # HELP
-@client.command(name="h")
-async def help(ctx, *specs):
+async def get_help_text(specs):
     if len(specs) == 0:
         em = await general_help()
-        await ctx.send(embed=em)
-        return
-    else:
-        if specs[0].lower() == "c":
+        return em
+    if specs[0].lower() == "c":
             em = await general_commands_help()
-            await ctx.send(embed=em)
-            return
-        elif specs[0].lower() == "mc":
+            return em
+    elif specs[0].lower() == "mc":
             em = await minecraft_help()
-            await ctx.send(embed=em)
-            return
+            return em
+    return discord.Embed()
+@client.command(name="help")
+async def help(ctx, *specs):
+    em = await get_help_text(specs)
+    await ctx.send(embed=em)
+    return
+@client.tree.command(
+    name="help",
+    description="learn about the commands!",
+)
+async def slash_help(ctx, specs: Optional[str]):
+    if specs == None:
+         em = await get_help_text([])
+    else:
+        em = await get_help_text([specs])
+    await ctx.response.send_message(embed=em)
     return
 
-
 # TIME
-@client.command(name="time")
-async def time(ctx):
+async def get_time():
     together = datetime.datetime(2024, 10, 20, 4, 31, 0)
     now = datetime.datetime.now()
     dif = relativedelta(now, together)
@@ -52,10 +55,28 @@ async def time(ctx):
             dif.minutes) + " minutes " + str(dif.seconds) + " seconds "
     em = discord.Embed(title="Kevin ♥ Jenny")
     em.add_field(name="We have been together for:", value=string)
-
+    return em
+@client.command(name="time")
+async def time(ctx):
+    em = await get_time()
     await ctx.send(embed=em)
     return
+@client.tree.command(
+          name="time",
+          description="How long have we been together?"
+)
+async def time_slash(ctx):
+    em = await get_time()
+    await ctx.response.send_message(embed=em)
+    return
 
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(
+        name="learn about commands with kj!help"))
+    await client.tree.sync()
+    #await client.tree.sync(guild=discord.Object(id=788204563173867540))
+    print("Ready!")
 
 keep_alive()
 client.run(os.environ['TOKEN'])
